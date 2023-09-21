98117753cbdb0e21dffb0ca6c21830b3a28d8f9d
Seok In Gyeong <dev.ingyeong@gmail.com>
2023-09-21 09:58:12 +0900

    create circle rectangle loop

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -6,5 +6,18 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 
+def run_circle():
+	print('CIRCLE')
+	pass
+
+def run_rectangle():
+	print('RECTANGLE')
+	pass
+
+while True:
+	run_circle()
+	run_rectangle()
+
+
 close_canvas()
 
