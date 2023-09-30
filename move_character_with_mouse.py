e5afb20cacceb578f07c29a8dd5aecad7c1424db
Seok In Gyeong <dev.ingyeong@gmail.com>
2023-10-01 00:11:04 +0900

    Update move_character_with_mouse.py

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,5 +1,4 @@
 from pico2d import *
-import random
 
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
@@ -12,57 +11,55 @@ hand_arrow = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x, y
+    global handx, handy
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        elif event.type == SDL_MOUSEMOTION:
+            handx, handy = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            point_x, point_y = event.x, TUK_HEIGHT - 1 - event.y
+            hand_point.append((point_x, point_y))
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-    pass
 
-
-def draw_line(x1, y1, x2, y2):
-    global startx, starty
-    global x,y
-    global t
-    global handx, handy
-
-    t += 0.001
-    x = (1 - t) * x1 + t * x2
-    y = (1 - t) * y1 + t * y2
-    if t > 1.0:
-        handx, handy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
-        startx , starty = x, y
-        t = 0
-
-def  decide_direction():
-    global direction
-    if handx < x:
-        direction = 0
-    else:
-        direction = 1
 
 
 running = True
 startx, starty = TUK_WIDTH // 2, TUK_HEIGHT // 2
 x, y = startx, starty
-direction = 0
+handx, handy = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 t = 0
 hide_cursor()
-handx , handy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
+hand_point = []
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    decide_direction()
-    draw_line(startx, starty, handx, handy)
     hand_arrow.draw(handx, handy)
-    character.clip_draw(frame * 100, 100 * direction, 100, 100, x, y)
-    update_canvas()
-    frame = (frame + 1) % 8
+    for arr in hand_point:
+        hand_arrow.draw(arr[0], arr[1])
 
+    if len(hand_point) == 0:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        frame = (frame + 1) % 8
+
+    elif hand_point and t < 1:
+        t += 0.001
+        x = (1 - t) * startx + t * hand_point[0][0]
+        y = (1 - t) * starty + t * hand_point[0][1]
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        frame = (frame + 1) % 8
+
+    if t > 1:
+        startx = x
+        starty = y
+        del hand_point[0]
+        t = 0
+
+    update_canvas()
     handle_events()
 
 close_canvas()
